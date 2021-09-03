91d7a3b9a49d748188d18f0a8ac9ec806a420f83
hgs-study <hgstudy_@naver.com>
2021-09-03 12:05:53 +0900

    [String] 3. 문장 속 단어

---
+++ src/com/string/Practice03.java
@@ -0,0 +1,44 @@
+package com.string;
+
+import java.util.Scanner;
+
+//3. 문장 속 단어
+//설명
+//한 개의 문장이 주어지면 그 문장 속에서 가장 긴 단어를 출력하는 프로그램을 작성하세요.
+//문장속의 각 단어는 공백으로 구분됩니다.
+//
+//입력
+//첫 줄에 길이가 100을 넘지 않는 한 개의 문장이 주어집니다. 문장은 영어 알파벳으로만 구성되어 있습니다.
+//
+//출력
+//첫 줄에 가장 긴 단어를 출력한다. 가장 길이가 긴 단어가 여러개일 경우 문장속에서 가장 앞쪽에 위치한
+//단어를 답으로 합니다.
+//
+//
+//예시 입력 1
+//it is time to study
+
+//예시 출력 1
+//study
+public class Practice03 {
+
+    public String solution(String sentence){
+        String[] units = sentence.split(" ");
+        int count = 0;
+
+        for (int i = 1; i < units.length; i++) {
+            if(units[i].length() > units[i-1].length())
+                count = i;
+        }
+
+        return units[count];
+    }
+
+    public static void main(String[] args) {
+        Practice03 practice03 = new Practice03();
+        Scanner scanner = new Scanner(System.in);
+        String sentence = scanner.nextLine();
+
+        System.out.println(practice03.solution(sentence));
+    }
+}
