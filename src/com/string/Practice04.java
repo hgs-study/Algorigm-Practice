ad5656ce5739791bad924da60e4554d75dcd5a0e
hgs-study <hgstudy_@naver.com>
2021-09-10 17:24:00 +0900

    [String] 단어 뒤집기

---
+++ src/com/string/Practice04.java
@@ -0,0 +1,55 @@
+package com.string;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+//4. 단어 뒤집기
+//설명
+//N개의 단어가 주어지면 각 단어를 뒤집어 출력하는 프로그램을 작성하세요.
+//
+//입력
+//첫 줄에 자연수 N(3<=N<=20)이 주어집니다.
+//두 번째 줄부터 N개의 단어가 각 줄에 하나씩 주어집니다. 단어는 영어 알파벳으로만 구성되어 있습니다.
+//
+//출력
+//N개의 단어를 입력된 순서대로 한 줄에 하나씩 뒤집어서 출력합니다.
+//
+//예시 입력 1
+//3
+//good
+//Time
+//Big
+
+//예시 출력 1
+//doog
+//emiT
+//giB
+public class Practice04 {
+
+    private void solution(String[] words){
+        Stack stack = new Stack();
+
+        for (int i = 0; i < words.length; i++) {
+            for (int j = 0; j < words[i].toCharArray().length; j++) {
+                stack.add(words[i].toCharArray()[j]);
+            }
+            while (!stack.isEmpty()){
+                System.out.print(stack.pop());
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Practice04 practice04 = new Practice04();
+        Scanner scanner = new Scanner(System.in);
+        int arraySize = scanner.nextInt();
+        String[] words = new String[arraySize];
+
+        for (int i = 0; i < arraySize; i++) {
+            words[i] = scanner.next();
+        }
+
+        practice04.solution(words);
+    }
+}
